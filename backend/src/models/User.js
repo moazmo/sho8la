f10f130c897dcb1cb4342cb8e71787dbff6a61aa
moazmo <your-email@example.com>
2025-12-06 22:57:25 +0200

    Fix: Improve password hashing pre-save hook error handling

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -18,9 +18,15 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 userSchema.methods.comparePassword = function (password) {
